01c9e664d939a5886debdcc41f22f8ed0559a886
Omnitok <94868134+Omnitok@users.noreply.github.com>
2023-04-19 12:22:58 +0200

    Add files via upload

---
+++ ptu_plot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 09:33:31 2023
+
+@author: Stenszky János
+Relative Humidity in respect ti ICE calculation
+"""
+import numpy as n
+from datetime import date
+
+file = '/home/stejan/Desktop/ballon/20211116_LTU18/ptu/ptu_20211116_data.txt'
+stime = date(2021,11,16,9,16,25)
